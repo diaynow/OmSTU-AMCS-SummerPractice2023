2c5b57c0d27ad6ba8d37e3bc3c4441629263f64e
diaynow <ekspert.d@bk.ru>
2023-06-28 19:42:50 +0000

    unittest

--- SquareEquation.Tests/SquareEquation.Test.cs
+++ SquareEquation.Tests/SquareEquation.Test.cs
@@ -7,21 +7,25 @@ namespace SquareEquation_Test
     {
 
     [Theory]
-    [InlineData(double.NaN,1,1)]
+    [InlineData(0,1,1)]
     [InlineData(1,double.NaN,1)]
-    [InlineData(1,1,double.NaN)]
-
-    [InlineData(double.PositiveInfinity,1,1)]
-    [InlineData(1,double.PositiveInfinity,1)]
     [InlineData(1,1,double.PositiveInfinity)]
-
     [InlineData(double.NegativeInfinity,1,1)]
-    [InlineData(1,double.NegativeInfinity,1)]
-    [InlineData(1,1,double.NegativeInfinity)]
-    public void testValidArgument(double a,double b, double c)
+    public void ErrorThrow(double a,double b, double c)
     {
         Assert.Throws<ArgumentException>(() => SquareEquation.Solve(a, b, c));
     }
+    [Theory]
+    [InlineData(1,1,1,new double[0])]
+    [InlineData(2,4,2,new double[1]{-1})]
+    [InlineData(1,4,2,new double[2]{-3,-1})]
+    public void NoErrorThrow(double a,double b,double c,double[] expected){
+        var result=SquareEquation.Solve(a,b,c);
+        Array.Sort(result);
+        Array.Sort(expected);
+        Array.Equals(result,expected);
+    }
+
 
 
 }
