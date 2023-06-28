b53c8dfa6d8dedb3081fd850c0972a2e0085aab2
diaynow <ekspert.d@bk.ru>
2023-06-28 10:39:46 +0000

    test

--- SquareEquation.Tests/SquareEquation.Test.cs
+++ SquareEquation.Tests/SquareEquation.Test.cs
@@ -1,17 +1,53 @@
 using Xunit;
 using SquareEquationLib;
 
-namespace SquareEquationTest
+namespace SquareEquation_Test
 {
-    public class SquareEquationLibUnit
+    public class SquareEquation_Test
     {
-        [Theory]
-        [InlineDate (0.1,1,1)]
-        [InlineDate (1,double.NaN,1)]
-        [InlineDate (1,1,double.PositiveInfinity)]
-        public void ThrowError(double a,double b,double c)
-        {
-            Assert.Throws<ArgumentException>(() => SquareEquation.Solve(a, b, c));
-        }
+
+    [Theory]
+    [InlineData(double.NaN,1,1)]
+    [InlineData(1,double.NaN,1)]
+    [InlineData(1,1,double.NaN)]
+
+    [InlineData(double.PositiveInfinity,1,1)]
+    [InlineData(1,double.PositiveInfinity,1)]
+    [InlineData(1,1,double.PositiveInfinity)]
+
+    [InlineData(double.NegativeInfinity,1,1)]
+    [InlineData(1,double.NegativeInfinity,1)]
+    [InlineData(1,1,double.NegativeInfinity)]
+    public void testValidArgument(double a,double b, double c)
+    {
+        Assert.Throws<ArgumentException>(() => SquareEquation.Solve(a, b, c));
     }
+
+    [Theory]
+        [InlineData(1, 6, 9, new double[]{-3})]
+        [InlineData(10, 2, 10, new double[] {})]
+        [InlineData(1,2,-3, new double[] {1, -3})]
+        [InlineData(1, 0, 1, new double[]{})]
+        [InlineData(1, 4, 5, new double[]{})]
+
+        public void ValidFindRoot(double a, double b, double c, double[] expected)
+        {
+            
+        double[] actual = SquareEquation.Solve(a, b, c);
+
+
+            Array.Sort(actual);
+            Array.Sort(expected);
+
+            if (expected.Length != actual.Length)
+            {
+                Assert.Fail("Amount of roots does not match");
+            }
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.Equal(expected[i], actual[i], 5);
+            }
+        }
+}
 }
