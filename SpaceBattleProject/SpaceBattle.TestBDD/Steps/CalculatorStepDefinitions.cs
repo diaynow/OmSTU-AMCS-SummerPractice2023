b5d8119579de62eb46361e855ebd9dab47487d55
diaynow <ekspert.d@bk.ru>
2023-07-04 16:10:32 +0000

    spacebattle

--- SpaceBattleProject/SpaceBattle.TestBDD/Steps/CalculatorStepDefinitions.cs
+++ SpaceBattleProject/SpaceBattle.TestBDD/Steps/CalculatorStepDefinitions.cs
@@ -1,58 +1,75 @@
+using System;
+using Xunit;
 using TechTalk.SpecFlow;
+using FluentAssertions;
+using SpaceBattle;
 
-namespace SpaceBattle.TestBDD.Steps
+namespace SpaceBattle.TestsBDD.Steps
 {
     [Binding]
-    public sealed class CalculatorStepDefinitions
+    public sealed class SpaceBattle_TestsBDD
     {
-       
-       // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
 
-       private readonly ScenarioContext _scenarioContext;
+        private SpaceShip _spaceShip = new SpaceShip();
+        private Exception _actualException = new Exception();
 
-       public CalculatorStepDefinitions(ScenarioContext scenarioContext)
-       {
-           _scenarioContext = scenarioContext;
-       }
 
-       [Given("the first number is (.*)")]
-       public void GivenTheFirstNumberIs(int number)
-       {
-           //TODO: implement arrange (precondition) logic
-           // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
-           // To use the multiline text or the table argument of the scenario,
-           // additional string/Table parameters can be defined on the step definition
-           // method. 
-
-           _scenarioContext.Pending();
-       }
-
-       [Given("the second number is (.*)")]
-       public void GivenTheSecondNumberIs(int number)
-       {
-           //TODO: implement arrange (precondition) logic
-           // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
-           // To use the multiline text or the table argument of the scenario,
-           // additional string/Table parameters can be defined on the step definition
-           // method. 
-
-           _scenarioContext.Pending();
+        [Given(@"космический корабль находится в точке пространства с координатами \((.*), (.*)\)")]
+        public void SpaceShipSetCoordinates(double x, double y)
+        {
+            _spaceShip.SetCoordinates(new double[2] { x, y });
         }
-        
-       [When("the two numbers are added")]
-       public void WhenTheTwoNumbersAreAdded()
-       {
-           //TODO: implement act (action) logic
 
-           _scenarioContext.Pending();
-       }
+        [Given(@"имеет мгновенную скорость \((.*), (.*)\)")]
+        public void SpaceShipSetSpeed(double x, double y)
+        {
 
-       [Then("the result should be (.*)")]
-       public void ThenTheResultShouldBe(int result)
-       {
-           //TODO: implement assert (verification) logic
+            _spaceShip.SetSpeed(new double[2] { x, y });
 
-           _scenarioContext.Pending();
-       }
+        }
+
+
+        [When(@"происходит прямолинейное равномерное движение без деформации")]
+        public void MovementSpaceShip()
+        {
+            try
+            {
+                _spaceShip.ChangeCoordinates();
+            }
+            catch (Exception a)
+            {
+                _actualException = a;
+            }
+        }
+        [Then(@"космический корабль перемещается в точку пространства с координатами \((.*), (.*)\)")]
+        public void SpaceShipMoveDot(double x, double y)
+        {
+            var newCoordinates = new double[] { x, y };
+            for (int i = 0; i < 2; i++)
+            {
+                Assert.Equal(_spaceShip.Coordinates[i], newCoordinates[i]);
+            }
+        }
+        [Given(@"космический корабль, положение в пространстве которого невозможно определить")]
+        public void UndefinedLocation()
+        {
+            _spaceShip.SetCoordinates(new double[2] { double.NaN, double.NaN });
+        }
+        [Given(@"скорость корабля определить невозможно")]
+        public void UndefinedSpeed()
+        {
+            _spaceShip.SetSpeed(new double[2] { double.NaN, double.NaN });
+        }
+        [Then(@"возникает ошибка Exception")]
+        public void ErrorException()
+        {
+            Assert.ThrowsAsync<Exception>(() => throw _actualException);
+        }
+        [Given(@"изменить положение в пространстве космического корабля невозможно")]
+        public void CantMove()
+        {
+            _spaceShip.Speed = new double[2] { double.NaN, double.NaN };
+        }
+
     }
 }
